9e7fa784a9939d107931cf9039bc04f653dd95af
Nishan Bajracharya <nisbaj11@gmail.com>
2021-03-28 16:20:56 +0545

    Add webpack optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+//   .BundleAnalyzerPlugin;
 
 const devOptions = {
   devtool: 'inline-source-map',
@@ -9,19 +11,20 @@ const devOptions = {
   },
 };
 
-const env = process.env.MODE || 'production'
+const env = process.env.MODE || 'production';
 
 console.log('[ENVIRONMENT]', env);
 
 function loadCustomConfig() {
-  return env === 'development' ? devOptions : {}
+  return env === 'development' ? devOptions : {};
 }
 
 module.exports = {
   mode: env,
   entry: './src/index.ts',
   output: {
-    filename: 'bundle.[name].[chunkhash].js',
+    filename: 'js/bundle.[name].[chunkhash].js',
+    chunkFilename: 'js/chunk.[name].chunkhash.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -54,6 +57,38 @@ module.exports = {
       template: path.resolve(__dirname, 'public/index.html'),
     }),
     new MonacoWebpackPlugin(),
+    // new BundleAnalyzerPlugin(),
   ],
-  ...loadCustomConfig()
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  ...loadCustomConfig(),
 };
