ae8b3b91c368646158bcec8ed179ec1f2bdda9e5
Nishan Bajracharya <nisbaj11@gmail.com>
2021-03-28 15:21:13 +0545

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+const devOptions = {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+};
+
+const env = process.env.MODE || 'production'
+
+console.log('[ENVIRONMENT]', env);
+
+function loadCustomConfig() {
+  return env === 'development' ? devOptions : {}
+}
+
+module.exports = {
+  mode: env,
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new MonacoWebpackPlugin(),
+  ],
+  ...loadCustomConfig()
+};
